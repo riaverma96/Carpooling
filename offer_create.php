d6041d4de2c7f81063415d13b5e33b66ff3b9e38
Nicholas <nlzz_22@hotmail.com>
2016-04-03 12:16:30 +0800

    Styled Create Offer page

--- offer_create.php
+++ offer_create.php
@@ -73,63 +73,66 @@ $numNotifications = pg_query($query);
 	
 	<!-- Offset Navbar -->
 <div style = "margin-top:70px;"></div>
-	
-	
-	
+	<div class="container">
 		<div>
-			<p><b>Create a new Offer</b></p>
+			<h1 class = "jumbotron"><b>Create a new Offer</b></h1>			
 			
-			<p><b>Select a car:</b></p>
-			<p>
-			<select name = "car" form="offer_create_form">
-			<?php 
-				$username = $_SESSION['login_user'];	
-				$query = "SELECT license from owns_car WHERE cOwner = '$username'";
-				$result = pg_query($query);
-				
-				if(pg_num_rows($result) == 0){
-					print "<option value=\"null\">No cars</option>";
-				} else{
-					while ($row = pg_fetch_array($result)){
-						print "<option value=\"$row[0]\">$row[0]</option>";
-					}
-				}
-			?>
-			</select>
-			</p>
-			
-		</div>
-		
-		
-		<div>		
-			<form action = "offer_create_script.php" id= "offer_create_form" method="post">
-				<p>
-				<label>Start Point:</label>
-				<input type="text" name="startPoint">
-				</p> <p>
-				<label>End Point:</label>
-				<input type="text" name="endPoint">
-				</p> <p>
-				<label>Number of Passengers:</label>
-				<input type="number" name="pax" min="1" value = "1">
-				</p> <p>
-				<label>Asking Price:</label>
-				<input type="number" name="price" step = "0.01" min="0.00" value = "0.00">
-				</p> <p>
-				<label>Date:</label>
-				<input type="date" name="date" value = "<?php echo date('Y-m-d'); ?>">
-				</p> <p>
-				<label>Time:</label>
-				<input type="number" name="hour" size = "2" min = "00" max = "23" value = "00">
-				<label>:</label>
-				<input type="number" name="minute" min = "00" max = "59" size = "2" value = "00">
-				</p><p>
-				<input name="submit" type="submit">
-				</p>
+			<form class="form-horizontal">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Select a car:</label>
+						<select class="form-control" style="width:200px;" name = "car" form="offer_create_form">
+						<?php 
+							$username = $_SESSION['login_user'];	
+							$query = "SELECT license from owns_car WHERE cOwner = '$username'";
+							$result = pg_query($query);
+							
+							if(pg_num_rows($result) == 0){
+								print "<option value=\"null\">No cars</option>";
+							} else{
+								while ($row = pg_fetch_array($result)){
+									print "<option value=\"$row[0]\">$row[0]</option>";
+								}
+							}
+						?>
+						</select>
+				</div>
 			</form>
 		</div>
 		
-		<a href="http://127.0.0.1/main.php">Back</a>
-
+		<div>		
+			<form class="form-horizontal" action = "offer_create_script.php" id= "offer_create_form" method="post">
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">Start Point:</label>
+					<input class="form-control" style="width:600px;" type="text" name="startPoint">
+				</div> 
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">End Point:</label>
+					<input class="form-control" style="width:600px;" type="text" name="endPoint">
+				</div>
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">Number of Passengers:</label>
+					<input class="form-control" style="width:100px;display:inline;" type="number" name="pax" min="1" value = "1">
+				</div> 
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">Asking Price:</label>
+					<input class="form-control" style="width:100px;display:inline;" type="number" name="price" step = "0.01" min="0.00" value = "0.00">
+				</div>
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">Date:</label>
+					<input class="form-control" style="width:200px;display:inline;" type="date" name="date" value = "<?php echo date('Y-m-d'); ?>">
+				</div>
+				<div class="form-group">
+					<label class = "col-sm-2 control-label">Time:</label>
+					<input class="form-control" style="width:100px;display:inline;" type="number" name="hour" size = "2" min = "00" max = "23" value = "00">
+					<label>:</label>
+					<input class="form-control" style="width:100px;display:inline;" type="number" name="minute" min = "00" max = "59" size = "2" value = "00">
+				</div>
+				<div class="col-sm-offset-1 col-sm-4" style = "margin-bottom:20px;">
+					<input class="btn btn-primary" style="width:100px;" name="submit" type="submit">
+					<input class="btn btn-danger" style="width:100px;" value="Back" onclick="window.location='http://127.0.0.1/main.php';"> 
+				</div>
+			</form>
+		</div>
+	</div>
 	</body>
 </html>
